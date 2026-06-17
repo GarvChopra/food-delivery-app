e9315dea9e0701b047fd83301335920141d4581d
GarvChopra <garvchopra0810@gmail.com>
2026-06-17 15:55:59 +0530

    connecting

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "styled-components"
+import { darkTheme } from './utlis/Theme'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <Navbar />
+  )
+}
+
+export default App
